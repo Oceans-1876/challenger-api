c6e17eddc2396c4badcbdc7608a3a4d8b014e93f
rashmil-1999 <damonsalvatore833@gmail.com>
2021-09-04 16:08:32 -0500

    Documented API's

--- app/utils/auth.py
+++ app/utils/auth.py
@@ -9,6 +9,18 @@ settings = get_settings()
 
 
 def generate_password_reset_token(email: str) -> str:
+    """generate_password_reset_token Generates the access token to reset/recover the password.
+
+    Parameters
+    ----------
+    email : str
+        Email ID of the User.
+
+    Returns
+    -------
+    str
+        Encoded JWT Token.
+    """
     delta = timedelta(hours=settings.EMAIL_RESET_TOKEN_EXPIRE_HOURS)
     now = datetime.utcnow()
     expires = now + delta
@@ -22,6 +34,18 @@ def generate_password_reset_token(email: str) -> str:
 
 
 def verify_password_reset_token(token: str) -> Optional[str]:
+    """verify_password_reset_token Check whether the token is authentic or not.
+
+    Parameters
+    ----------
+    token : str
+        Password reset/recover token.
+
+    Returns
+    -------
+    Optional[str]
+        Email ID in the token or None.
+    """
     try:
         decoded_token = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
         return decoded_token["email"]

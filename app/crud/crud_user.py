3d992b43567f47a3e7d32c1c2d0da49f610ad1b4
kaveh <kaveh@illinois.edu>
2021-09-08 14:38:27 -0500

    Add support for importing stations and species

--- app/crud/crud_user.py
+++ app/crud/crud_user.py
@@ -12,7 +12,7 @@ class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
     def get_by_email(self, db: Session, *, email: str) -> Optional[User]:
         return db.query(User).filter(User.email == email).first()
 
-    def create(self, db: Session, *, obj_in: UserCreate) -> User:
+    def create(self, db: Session, *, obj_in: UserCreate) -> User:  # type: ignore[override] # noqa E501
         db_obj = User(
             email=obj_in.email,
             hashed_password=get_password_hash(obj_in.password),

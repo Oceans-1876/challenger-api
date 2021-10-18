faa7964f05bceee21ac0b552cf86cd17142e4ee2
rashmil-1999 <damonsalvatore833@gmail.com>
2021-10-18 15:06:45 -0500

    made olajides changes

--- app/tests/api/api_v1/test_species.py
+++ app/tests/api/api_v1/test_species.py
@@ -1,25 +1,29 @@
 from typing import Dict
 from fastapi.testclient import TestClient
-from sqlalchemy.orm import Session
-from app import crud, schemas
 from fastapi import APIRouter, Depends
+
+from sqlalchemy.orm import Session
+
+from app import crud, schemas
+from app.core.config import get_settings
+
 import requests
 
+settings = get_settings()
+
 from fastapi import FastAPI, Request
-def test_read_species(
-    client: TestClient, skip, limit: int, db: Session
-) -> None:
-    #data = {"email": username, "password": password}
-    r = client.get(
-        f"/species.json/",
-        skip=skip, limit=limit,
-    )
+
+
+def test_read_species(client: TestClient, db: Session) -> None:
+    # data = {"email": username, "password": password}
+    r = client.get(f"{settings.API_V1_STR}/species/")
     assert 200 <= r.status_code < 300
-    #created_user = r.json()
-    species = crud.species.get_multi(db, skip=skip, limit=limit)
+    # created_user = r.json()
+    species = crud.species.get_multi(db)
     assert species
 
-'''def test_create_user_new_email(
+
+"""def test_create_user_new_email(
     client: TestClient, superuser_token_headers: dict, db: Session
 ) -> None:
     username = random_email()
@@ -34,4 +38,4 @@ def test_read_species(
     created_user = r.json()
     user = crud.user.get_by_email(db, email=username)
     assert user
-    assert user.email == created_user["email"]'''
+    assert user.email == created_user["email"]"""

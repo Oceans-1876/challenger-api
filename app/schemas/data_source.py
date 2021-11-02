04cab177325ae7532535c84ccea74b1f39607172
rashmil-1999 <damonsalvatore833@gmail.com>
2021-11-02 13:58:20 -0500

    Add Data Source Model and Schema

--- app/schemas/data_source.py
+++ app/schemas/data_source.py
@@ -1,7 +1,8 @@
 """Pydantic models for representing data sources used by
 [Global Names](https://verifier.globalnames.org/data_sources).
 """
-from typing import List
+from datetime import date  # noqa
+from typing import List, Optional
 
 from pydantic import BaseModel
 
@@ -13,20 +14,40 @@ class DataSourceBase(BaseModel):
     title: str
 
 
-class DataSourceCreate(DataSourceBase):
+class DataSourceSummaryInDB(DataSourceBase):
+    class Config:
+        orm_mode = True
+
+
+class DataSourceSummary(DataSourceSummaryInDB):
     pass
 
 
-class DataSourceUpdate(DataSourceBase):
+class DataSourceDetailsBase(DataSourceBase):
+    title_short: str
+    description: Optional[str]
+    curation: str
+    record_count: Optional[int]
+    updated_at: date
+    is_out_link_ready: bool
+    home_url: Optional[str]
+    url_template: Optional[str]
+
+
+class DataSourceCreate(DataSourceDetailsBase):
     pass
 
 
-class DataSourceInDBIDOnly(DataSourceBase):
+class DataSourceUpdate(DataSourceDetailsBase):
+    pass
+
+
+class DataSourceDetailsInDB(DataSourceDetailsBase):
     species: List[SpeciesID]
 
     class Config:
         orm_mode = True
 
 
-class DataSourceIDOnly(DataSourceInDBIDOnly):
+class DataSourceDetails(DataSourceDetailsInDB):
     pass

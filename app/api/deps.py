72b7d9a7921b2a1656b55a632b5d652663ef8ac9
rashmil-1999 <damonsalvatore833@gmail.com>
2021-12-04 19:14:35 -0600

    Remove unused imports

--- app/api/deps.py
+++ app/api/deps.py
@@ -1,4 +1,4 @@
-from typing import Generator, Optional
+from typing import Generator
 
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 

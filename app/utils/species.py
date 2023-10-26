043f5003ca113f285ee76b6a493fb62d2f561c43
Wenqi He <hewenqi96@gmail.com>
2023-10-26 18:57:08 -0500

    filter species at api endpoints

---
+++ app/utils/species.py
@@ -0,0 +1,13 @@
+from typing import List
+
+from app.models import Species
+
+
+def binomial_only(species: List[Species]) -> List[Species]:
+    """
+    TODO: This is a temporary fix. In the future, we could consider filtering out genera
+    when loading data into the database or as part of the OCR workflow.
+    """
+    # Unlike `current_name`, `current_canonical_full_name` doesn't include
+    # author(s) and year of publication, so for a genus, there won't be any spaces.
+    return [sp for sp in species if " " in sp.current_canonical_full_name]
